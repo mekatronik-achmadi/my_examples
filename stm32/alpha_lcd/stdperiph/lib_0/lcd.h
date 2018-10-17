0583c14eb44f993bfcea053433abf82810fdc023
mekatronik-achmadi <mekatronik.achmadi@gmail.com>
2018-10-17 15:07:13 +0700

    lcd library

--- stm32/alpha_lcd/stdperiph/lib_0/lcd.h
+++ stm32/alpha_lcd/stdperiph/lib_0/lcd.h
@@ -3,14 +3,15 @@
 
 #include "stm32f10x_conf.h"
 
-#define tunda 0x3FFF
-
 void LCD_DATA_DIR_OUT(void);
 void Delay(__IO uint32_t nCount);
 void Lcd_Write_Command(uint8_t cmd);
 void Lcd_Write_Data(uint8_t ch);
-void Lcd_Setup(void);
+void Lcd_Init(void);
 void Lcd_Cursor(uint8_t column, uint8_t line);
-void Lcd_Clear(void);
- 
+void Lcd_Print (char *string);
+void Lcd_Clear (void);
+
+void Example_LCD1602(void);
+
 #endif
