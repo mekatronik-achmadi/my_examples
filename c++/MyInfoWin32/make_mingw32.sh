01e9cf1c7d8065c35010d208792bdcea9f417841
mekatronik-achmadi <mekatronik.achmadi@gmail.com>
2018-12-13 14:39:40 +0700

    mingw makefile

--- c++/MyInfoWin32/make_mingw32.sh
+++ c++/MyInfoWin32/make_mingw32.sh
@@ -24,10 +24,17 @@ if [ -z "$EXENAME" ];then
 	exit
 fi
 
-winemaker --lower-all --lower-include --nomfc --windows --wine32 -luser32 .
+winemaker --lower-all --lower-include --nomfc --wine32 -luser32 .
+
 export OUTNAME=$(cat Makefile | grep -w "EXES                  =" | cut -f2 -d\= | tr -d '[:space:]')
+
 sed -i "s#$OUTNAME#$EXENAME.exe#g" Makefile
+
 sed -i "s#winegcc#i686-w64-mingw32-gcc#g" Makefile
 sed -i "s#wineg++#i686-w64-mingw32-g++#g" Makefile
+
+sed -i "s#AR = ar#AR = i686-w64-mingw32-ar#g" Makefile
+sed -i "s#RC = wrc#RC = i686-w64-mingw32-windres#g" Makefile
+
 sed -i "s#-mno-cygwin#-static-libgcc#g" Makefile
 # make
