c579848fb0eeeb2e4c31618ed18605c573a8a0d2
mekatronik-achmadi <mekatronik.achmadi@gmail.com>
2021-01-07 13:36:46 +0700

    SQL python

--- python/general/mysql.py
+++ python/general/mysql.py
@@ -17,6 +17,31 @@ dbcsr.execute("CREATE DATABASE sensor")
 # Get all databases into buffer
 dbcsr.execute("SHOW DATABASES")
 
-## 'fetchall()' method fetches all the rows from the last executed statement
+# 'fetchall()' method fetches all the rows from the last executed statement
 dbases = dbcsr.fetchall()
 print(dbases)
+
+###############################
+
+# access spesific database
+tbl = mysql.connect(host = "localhost",user = "sqluser",passwd = "",database = "sensor")
+tblcsr = tbl.cursor()
+
+tblcsr.execute("CREATE TABLE adc(date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, v0 INT(16))")
+
+###############################
+
+tblcsr.execute("INSERT INTO adc(v0) VALUES (%s)" % (str(100)))
+tbl.commit()
+
+###############################
+
+tblcsr.execute("SELECT * FROM adc")
+records = tblcsr.fetchall()
+for record in records:
+	print(record)
+
+tblcsr.execute("SELECT v0 FROM adc")
+records = tblcsr.fetchall()
+for record in records:
+	print(record)
