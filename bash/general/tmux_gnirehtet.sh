228717fe945febdd9866e5ed732189b204de771e
mekatronik-achmadi <mekatronik.achmadi@gmail.com>
2021-04-19 15:25:38 +0700

    some scripts

--- bash/general/tmux_gnirehtet.sh
+++ bash/general/tmux_gnirehtet.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-################# check binary #################
 tmux_path=$(command -v tmux)
 if [ -z "$tmux_path" ];then echo "tmux not found";exit;fi
 
@@ -13,17 +12,14 @@ if [ -z "$gniht_path" ];then echo "r_gnirehtet not found";exit;fi
 scrcpy_path=$(command -v scrcpy)
 if [ -z "$scrcpy_path" ];then echo "scrcpy not found";exit;fi
 
-################# start new tmux session #################
+#adb kill-server
+sudo adb start-server
+
 session="gnirehtet"
 tmux new -d -s $session
-
-################# split window #################
 tmux split-window -v -t $session
 
-################# start programs in each pane #################
-sudo adb start-server
 tmux send-keys -t $session:0.0 'r_gnirehtet' C-m
 tmux send-keys -t $session:0.1 'scrcpy -S -m1024' C-m
 
-################# start programs in each pane #################
 tmux attach -t $session
